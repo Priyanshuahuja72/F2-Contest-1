023b97e4284b4679568269cb83bfc132882c7faf
Priyanshu Ahuja <85331994+Priyanshuahuja72@users.noreply.github.com>
2023-02-11 13:19:57 +0530

    sec-one

--- script.js
+++ script.js
@@ -8,20 +8,50 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+   arr.map(({id , name , age , marks}) => {
+    if(marks > 50)
+    {
+      console.log(id , name , age , marks);
+    }
+  })
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+    arr.forEach(({id , name , age , marks }) => {
+    if(marks > 50)
+    {
+      console.log(id , name , age ,marks);
+    }
+  })
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newData = {id:4,name:"susan",age:"20",marks:45};
+  arr.push(newData);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  for(var i = 0 ; i < arr.length ; i++)
+  {
+    if(arr[i].marks < 50)
+    {
+      arr.splice(i);
+    }
+  }
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr2 = [
+    { id: 5, name: "jerry", age: "18", marks: 80 },
+    { id: 6, name: "raman", age: "20", marks: 85 },
+    { id: 7, name: "hari", age: "19", marks: 35 },
+  ];
+  let children = arr2.concat(arr);
+  console.log(children);
 }
